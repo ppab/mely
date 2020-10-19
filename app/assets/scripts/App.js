f3aafe131f1864976d12df7d2f39b3a5a92eda3e
ppab <josearmentab@gmail.com>
2020-10-19 18:54:00 -0500

    subnav component ready

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,16 +2,21 @@
 import '../css/styles.css';
 
 
+//js modules
 
- //React Related Code Goes Here
+import subNav from '../scripts/subNav'
+const subnav=subNav();
+
+
+
+ //React Related Code Goes Herec
  import React from 'react';
  import ReactDOM from 'react-dom';
 
 
- ReactDOM.render(<Component1/>,document.querySelector("#react"));
+//  ReactDOM.render(<Component1/>,document.querySelector("#react"));
 
 
 if (module.hot){
     module.hot.accept();
 }
-
