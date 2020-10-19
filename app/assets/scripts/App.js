4aaba8032c1649a01ff92b4151b2491c567e58c3
ppab <josearmentab@gmail.com>
2020-10-18 20:11:23 -0500

    first commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,17 @@
+
+import '../css/styles.css';
+
+
+
+ //React Related Code Goes Here
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+
+
+ ReactDOM.render(<Component1/>,document.querySelector("#react"));
+
+
+if (module.hot){
+    module.hot.accept();
+}
+
