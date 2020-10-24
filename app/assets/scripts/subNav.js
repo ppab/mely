19e247a5e2534952e7dc79d6a9625cfd6b9b4d38
ppab <josearmentab@gmail.com>
2020-10-23 19:04:56 -0500

    image-text-row v1

--- app/assets/scripts/subNav.js
+++ app/assets/scripts/subNav.js
@@ -3,10 +3,11 @@ const subNav= () =>
 {
 
     const buttonParent = document.querySelectorAll('.subnav__button');
-
+    
 
     buttonParent.forEach(element => {
         element.addEventListener("click", function() {
+
             element.nextElementSibling.classList.toggle('subnav__items_container--show');
             element.firstElementChild.classList.toggle('subnav__arrow-down--not-shown');
             element.lastElementChild.classList.toggle('subnav__arrow-up--shown');
